f0b2025985658ae0ac1ec4ec8564ee329cb9d317
JotaDD <joaoeduardogomes@gmail.com>
2024-05-03 16:58:10 -0300

    refactor: header max width

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return (
     <header className="flex w-full min-w-fit items-center justify-center  bg-ic-stone-200">
-      <div className="max-w-screen  flex w-[80%] items-center justify-between py-2">
+      <div className="flex w-[80%] max-w-[1200px]  items-center justify-between  py-2">
         <Logo />
         <div className="flex w-fit gap-2  md:order-2 md:space-x-8 rtl:space-x-reverse ">
           <TranslateButton />
